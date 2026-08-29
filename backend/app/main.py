d99be4ee69c62fa9b7fc719889ed13960c133f78
Your Name <priyanshuanand471@gmail.com>
2026-08-29 16:56:07 +0530

    Fix CORS for deployed frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,16 +17,13 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-
     allow_origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "https://smartvision-frontend.onrender.com",
     ],
-
     allow_credentials=True,
-
     allow_methods=["*"],
-
     allow_headers=["*"],
 )
 
